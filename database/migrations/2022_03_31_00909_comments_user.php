846fb9e4251d1401b0655ac6a199a6ca383f63c3
SiyaSalekar <92158161+SiyaSalekar@users.noreply.github.com>
2022-03-31 14:14:39 +0100

    Database creation

---
+++ database/migrations/2022_03_31_00909_comments_user.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Posts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments_users', function (Blueprint $table){
+            $table->increments('comment_user_id');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('comment_id');
+            $table->foreign('comment_id')->references('comment_id')->on('comments');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
