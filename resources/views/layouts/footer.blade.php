9d89edc442458cbd62e79aba4cdbe81580937a22
Luana Kimley <luanakimley@gmail.com>
2022-04-09 19:39:17 +0100

    some modifications to footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,21 +3,24 @@
     <div class="container p-6">
        
       <div class="">
-        <img
-            src="https://media.discordapp.net/attachments/902133591272730654/961667287419617351/Explore_new_opportunities.png" class="max-w-full h-20 rounded-full" alt="">
+
         <p class="flex justify-center items-center">
-          <p class="font-mono">Our Blogs inspire you to achieve your Dreams!</p>
+          <img
+            src="https://media.discordapp.net/attachments/902133591272730654/961667287419617351/Explore_new_opportunities.png" class="mt-3 max-w-full h-20 rounded-full mb-7" alt="">
+          <p class="font-mono">Our blog will inspire you to achieve your dreams!</p>
             <br>
+
+            @if (Auth::guest())
           
           <span class="mr-4">Register for free</span>
           <button type="button" class="inline-block px-6 py-2 border-2 border-white text-white font-medium text-xs leading-tight uppercase rounded-full hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out">
             
-            @if (Route::has('register'))
+          
                 <a class="no-underline hover:underline" href="{{ route('register') }}">{{ __('Register') }}</a>
             @endif
         
           </button>
-          <br>
+
         </p>
       </div>
     </div>
